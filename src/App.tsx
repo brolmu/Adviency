a910714f480f84f36b45bf4b368b2d6b34d0e0eb
brolmu <54647953+brolmu@users.noreply.github.com>
2022-11-29 10:14:38 -0400

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Lista de Regalos</h1>
+      <ul>
+        <li>Laptop</li>
+        <li>Telefono</li>
+        <li>Televisor</li>
+      </ul>
+    </div>
+  );
+}
